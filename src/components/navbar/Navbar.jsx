d9cb9a069b67603d82dbe54f42b76c8f91255a2d
shamimdev2000 <mdshamim.en@gmail.com>
2026-03-05 12:57:57 +0600

    navbar done

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import logo from "../../assets/logo.png"
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { FaInstagram,FaLinkedinIn,FaTwitter,FaFacebookF,} from "react-icons/fa";
+import { RiMenu3Fill } from "react-icons/ri";
+
+
+const Navbar = () => {
+  return (
+ <nav className='bg-[#f4f4f4] py-[15px] font-Rajdhani py-4'>
+  <div className='container flex justify-between mx-auto  '>
+    <div>
+        <img src={logo} alt="" />
+    </div>
+
+
+    <div className='flex items-center gap-x-[30px] font-bold'>
+        <div>Home</div>
+        <div>About</div>
+        <div className='flex items-center gap-x-0.5'>Services <MdKeyboardArrowDown /></div>
+        <div className='flex items-center gap-x-0.5'>Blog <MdKeyboardArrowDown /></div>
+        <div className='flex items-center gap-x-0.5'>Project <MdKeyboardArrowDown /></div>
+        <div>Contact</div>
+    </div>
+
+
+
+    <div className='flex items-center gap-x-2'> 
+        <div className='py-[13px] px-[13px] rounded-[50px] bg-[#E7E7E7]'><FaInstagram /></div>
+        <div className='py-[13px] px-[13px] rounded-[50px] bg-[#E7E7E7]'><FaLinkedinIn /></div>
+        <div className='py-[13px] px-[13px] rounded-[50px] bg-[#E7E7E7]'><FaTwitter /></div>
+        <div className='py-[13px] px-[13px] rounded-[50px] bg-[#E7E7E7]'><FaFacebookF /></div>
+        <div className='py-[13px] px-[13px] ml-[25px] rounded-[50px] bg-[#FF494A] text-white text-[20px]'><RiMenu3Fill /></div>
+    </div>
+
+    </div>
+
+  </nav>
+
+ 
+  )
+}
+
+export default Navbar
